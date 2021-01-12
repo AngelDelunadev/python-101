c4eeca1a1f3b0d24ee438f8bdbe177bb40e70a61
Angel Deluna <angelluna2016@gmail.com>
2021-01-12 16:52:37 -0500

    added python code

---
+++ madlib.py
@@ -0,0 +1,5 @@
+print("Please fill in the blanks below: ")
+print("__(name)___'s favorite subject in school is __(subject)___.")
+name = input("What is the name? ")
+subject= input("what is the subject? ")
+print("%s's favorite subject in school is %s."%(name, subject))
